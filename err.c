ad33b1baf6fa6a7cc2d8bd9e30da0cdc3e88d201
Shaker Sharabi <sheko100@icloud.com>
2023-08-25 05:40:50 +0300

    Add err.c file

---
+++ err.c
@@ -0,0 +1,50 @@
+#include "monty.h"
+
+/**
+ * errusage - prints an error to stderr when the arguments are out of range
+ *
+ * Return: void
+ */
+void errusage(void)
+{
+	fprintf(stderr, "USAGE: monty file\n");
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * errmalloc - prints an error to stderr when malloc fails
+ *
+ * Return: void
+ */
+void errmalloc(void)
+{
+	fprintf(stderr, "Error: malloc failed\n");
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * erropcode - prints an error to stderr when the opcode is invalid
+ * @opcode: pointer to the structure of opcode
+ * @linenum: the number of the line
+ *
+ * Return: void
+ */
+void erropcode(instruction_t *opcode, int linenum)
+{
+	fprintf(stderr, "L%d: unknown instruction %s\n", linenum, opcode->opcode);
+	free(opcode);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * errpush - prints an error to stderr when the argument is not integer
+ * @linenum: the line of the number
+ *
+ * Return: void
+ */
+
+void errpush(int linenum)
+{
+	fprintf(stderr, "L%d: usage: push integer\n", linenum);
+	exit(EXIT_FAILURE);
+}
